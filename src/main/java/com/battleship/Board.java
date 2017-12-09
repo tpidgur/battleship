69cb257a4e182d3ef63727613d4f3cb827159ce5
Tetiana Pidhurska <tetiana_pidhurska@epam.com>
2017-12-09 12:37:25 +0200

    Add functionality of placing ships at the board

--- src/main/java/com/battleship/Board.java
+++ src/main/java/com/battleship/Board.java
@@ -2,9 +2,34 @@ package com.battleship;
 
 public class Board {
 
-    private int[][] field = new int[10][10];
+    private int[][] cells = new int[10][10];
+    private static final int EMPTY = 0;
+    private static final int OCCUPIED = 1;
+    private static final int START_POINT_INDEX = 0;
+    private static final int END_POINT_INDEX = 1;
+    private static final int SINGLE_POINT = 1;
 
-    public boolean checkHitStatus(int coordinateX, int coordinateY) {
-        return field[coordinateX][coordinateY] != 0;
+    public boolean isEmptyCell(Point point) {
+        return cells[point.getX()][point.getY()] == EMPTY;
+    }
+
+    public void placeShip(Point... points) {
+        if (points.length == SINGLE_POINT) {
+            placeSingleDeckShip(points[START_POINT_INDEX]);
+        } else {
+            placeMultiDeckShip(points[START_POINT_INDEX], points[END_POINT_INDEX]);
+        }
+    }
+
+    private void placeSingleDeckShip(Point point) {
+        cells[point.getX()][point.getY()] = OCCUPIED;
+    }
+
+    private void placeMultiDeckShip(Point startPoint, Point endPoint) {
+        for (int x = startPoint.getX(); x <= endPoint.getX(); x++) {
+            for (int y = startPoint.getY(); y <= endPoint.getY(); y++) {
+                cells[x][y] = OCCUPIED;
+            }
+        }
     }
 }
--- src/test/java/com/battleship/BoardTest.java
+++ src/test/java/com/battleship/BoardTest.java
@@ -1,16 +1,54 @@
 package com.battleship;
 
+
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class BoardTest {
 
-    private Board board = new Board();
+    private Board board;
+    private static final Point A = new Point(1, 1);
+    private static final Point B = new Point(1, 2);
+    private static final Point C = new Point(1, 3);
+    private static final Point D = new Point(1, 4);
+    private static final Point E = new Point(2, 1);
+    private static final Point F = new Point(3, 1);
+    private static final Point G = new Point(4, 1);
+
+    @Before
+    public void setup() {
+        board = new Board();
+    }
 
     @Test
-    public void shouldMissContender() {
-        boolean hitStatus = board.checkHitStatus(1, 1);
-        assertTrue(hitStatus == false);
+    public void shouldCheckCellStatus() {
+        assertTrue(board.isEmptyCell(new Point(1, 1)));
+    }
+
+    @Test
+    public void shouldPlaceOneDeckShip() {
+        board.placeShip(A);
+        assertFalse(board.isEmptyCell(A));
+    }
+
+    @Test
+    public void shouldPlaceFourDeckShipVertically() {
+        board.placeShip(A, D);
+        assertFalse(board.isEmptyCell(A));
+        assertFalse(board.isEmptyCell(B));
+        assertFalse(board.isEmptyCell(C));
+        assertFalse(board.isEmptyCell(D));
+    }
+
+    @Test
+    public void shouldPlaceFourDeckShipHorizontally() {
+        board.placeShip(A, G);
+        assertFalse(board.isEmptyCell(A));
+        assertFalse(board.isEmptyCell(E));
+        assertFalse(board.isEmptyCell(F));
+        assertFalse(board.isEmptyCell(G));
     }
 }
